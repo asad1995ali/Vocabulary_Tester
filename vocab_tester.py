73ea9e97539e1222f19193ce4d372f92d633c8d7
asad1995ali <57779072+asad1995ali@users.noreply.github.com>
2020-11-03 00:02:54 +0100

    Update vocab_tester.py
    cleaned up the code

--- vocab_tester.py
+++ vocab_tester.py
@@ -4,36 +4,24 @@ from tkinter import *
 from pandas import *
 
 
-# generate random number
-def rand_num(length):
-    n = random.randint(0, length)
-    return n
-
-
 # take data from excel file and return value to text field
 def word_generator(a):
     df = pandas.read_excel('Vocabulary.xlsx', sheet_name='Sheet1')
     words = df['Word'].tolist()
     meanings = df['Meaning'].tolist()
 
-    n = rand_num(len(words))
-
     if a == 1:
         t1.delete(1.0, END)
-        t1.insert(END, words[n])
         t2.delete(1.0, END)
+        t1.insert(END, words[random.randint(0, len(words))])
 
     if a == 2:
-        t1_value = str(t1.get(1.0, END)).rstrip("\n")
         t2.delete(1.0, END)
-        t2.insert(END, meanings[words.index(t1_value)])
+        t2.insert(END, meanings[words.index(str(t1.get(1.0, END)).rstrip("\n"))])
 
 
 # GUI Setup
 window = Tk()
-
-t1_value = StringVar()
-
 
 b1 = Button(window, text="Worte", command=lambda: word_generator(1))
 b2 = Button(window, text="Bedeutung", command=lambda: word_generator(2))
@@ -46,6 +34,4 @@ t2.grid(row=1, column=3, rowspan=2)
 t1 = Text(window, height=2, width=30)
 t1.grid(row=1, column=1, rowspan=2)
 
-t1_value = str(t1.get(1.0, END))
-print(t1_value)
 window.mainloop()
